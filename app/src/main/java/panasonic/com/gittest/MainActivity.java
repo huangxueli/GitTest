783024907ece7fc40206205b1638106d067717d6
huangxueli <328669873@qq.com>
2017-06-12 15:29:32 +0800

    Merge branch 'branch1'
    # Conflicts:
    #	app/src/main/java/panasonic/com/gittest/MainActivity.java

--- app/src/main/java/panasonic/com/gittest/MainActivity.java
+++ app/src/main/java/panasonic/com/gittest/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        // 修复Bug
+        // 修复Bug2
         return true;
     }
 
