dc4809d7572ca5d5ccb41d228dbedc256a1cee71
huangxueli <328669873@qq.com>
2017-06-12 15:27:36 +0800

    修复bug

--- app/src/main/java/panasonic/com/gittest/MainActivity.java
+++ app/src/main/java/panasonic/com/gittest/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        // 修复Bug
         return true;
     }
 
